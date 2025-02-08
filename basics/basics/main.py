6f1885c956a11dab11b78873095ab06bf05c678e
xpakx <55586470+xpakx@users.noreply.github.com>
2025-02-08 20:02:55 +0100

    feat: create basic chat

--- basics/basics/main.py
+++ basics/basics/main.py
@@ -43,8 +43,45 @@ def stream_deepseek(client: OpenAI, content: str) -> Generator[str, None, None]:
         raise
 
        
-client = get_client()
-print(ask_deepseek("Hello!"))
+def test(client: OpenAI):
+    print(ask_deepseek("Hello!"))
 
-for chunk in stream_deepseek(client, "Hello from python!"):
-    print(chunk, end="", flush=True)
+    for chunk in stream_deepseek(client, "Hello from python!"):
+        print(chunk, end="", flush=True)
+
+
+def stream_chat(client: OpenAI, messages) -> Generator[str, None, None]:
+    try:
+        completion = client.chat.completions.create(
+            model="deepseek/deepseek-chat:free",
+            messages=messages,
+            stream=True
+        )
+
+        for chunk in completion:
+            if chunk.choices[0].delta.content:
+                yield chunk.choices[0].delta.content
+    except Exception as e:
+        print(f"An error occurred while asking DeepSeek: {e}")
+        raise
+
+
+def chat(client: OpenAI):
+    query: str = ""
+    history = []
+    while True:
+        query = input("Input: ")
+        if query == "":
+            break
+        history.append({"role": "user", "content": query})
+        response = ""
+        for chunk in stream_chat(client, history):
+            print(chunk, end="", flush=True)
+            response += chunk
+        print("\n");
+        history.append({"role": "assistant", "content": response})
+
+if __name__ == "__main__":
+    client = get_client()
+    # test(client)
+    chat(client)
