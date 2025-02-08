0c745cd20dcd320f6b359d32d951a51b137ed053
xpakx <55586470+xpakx@users.noreply.github.com>
2025-02-08 19:35:04 +0100

    refactor: add type annotations

--- basics/basics/main.py
+++ basics/basics/main.py
@@ -1,19 +1,20 @@
 from openai import OpenAI
 import json
+from typing import Dict, Any
 
 
-def load_config(filename: str):
+def load_config(filename: str) -> Dict[str, Any]:
     with open(filename, 'r') as file:
         return json.load(file)
 
 
-def get_client():
+def get_client() -> OpenAI:
     config = load_config("config.json");
     url = "https://openrouter.ai/api/v1"
     return OpenAI(base_url=url, api_key=config["apiKey"])
 
 
-def ask_deepseek(content: str):
+def ask_deepseek(content: str) -> str:
     messages = [{"role": "user", "content": content}]
     completion = client.chat.completions.create(
             model="deepseek/deepseek-chat:free",
