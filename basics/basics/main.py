c3c91b442a4a5bf0292768e3ef921abd948efb2b
xpakx <55586470+xpakx@users.noreply.github.com>
2025-02-08 20:16:04 +0100

    feat: let user edit input in vim

--- basics/basics/main.py
+++ basics/basics/main.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 import json
 from typing import Generator, Dict, Any
+import tempfile
+import subprocess
 
 
 def load_config(filename: str) -> Dict[str, Any]:
@@ -66,11 +68,23 @@ def stream_chat(client: OpenAI, messages) -> Generator[str, None, None]:
         raise
 
 
+def get_input(prompt="Enter message:"):
+    query = input(prompt + " ")
+    if query != "":
+        return query
+    with tempfile.NamedTemporaryFile(suffix=".txt", mode='w+') as temp_file:
+        subprocess.call(['nvim', temp_file.name])
+        temp_file.seek(0)
+        user_input = temp_file.read().strip()
+    return user_input
+
+
 def chat(client: OpenAI):
     query: str = ""
     history = []
+    response = ""
     while True:
-        query = input("Input: ")
+        query = get_input()
         if query == "":
             break
         history.append({"role": "user", "content": query})
@@ -83,5 +97,4 @@ def chat(client: OpenAI):
 
 if __name__ == "__main__":
     client = get_client()
-    # test(client)
     chat(client)
