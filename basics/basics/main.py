96e1a4c7af3c2ccb5734f0cfb492de2ea1567e4d
xpakx <55586470+xpakx@users.noreply.github.com>
2025-02-08 19:35:16 +0100

    feat: stream response from llm

--- basics/basics/main.py
+++ basics/basics/main.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 import json
-from typing import Dict, Any
+from typing import Generator, Dict, Any
 
 
 def load_config(filename: str) -> Dict[str, Any]:
@@ -16,12 +16,35 @@ def get_client() -> OpenAI:
 
 def ask_deepseek(content: str) -> str:
     messages = [{"role": "user", "content": content}]
-    completion = client.chat.completions.create(
+    try:
+        completion = client.chat.completions.create(
+                model="deepseek/deepseek-chat:free",
+                messages=messages
+        )
+        return completion.choices[0].message.content
+    except Exception as e:
+        print(f"An error occurred while asking DeepSeek: {e}")
+        raise
+
+def stream_deepseek(client: OpenAI, content: str) -> Generator[str, None, None]:
+    messages = [{"role": "user", "content": content}]
+    try:
+        completion = client.chat.completions.create(
             model="deepseek/deepseek-chat:free",
-            messages=messages
-    )
-    return completion.choices[0].message.content
+            messages=messages,
+            stream=True
+        )
+
+        for chunk in completion:
+            if chunk.choices[0].delta.content:
+                yield chunk.choices[0].delta.content
+    except Exception as e:
+        print(f"An error occurred while asking DeepSeek: {e}")
+        raise
 
        
 client = get_client()
 print(ask_deepseek("Hello!"))
+
+for chunk in stream_deepseek(client, "Hello from python!"):
+    print(chunk, end="", flush=True)
