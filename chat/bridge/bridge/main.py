2e61609babd8b463f2e87bb43b4e1dbc84c6f1c8
xpakx <55586470+xpakx@users.noreply.github.com>
2025-02-09 15:56:51 +0100

    fix: remove client from subscription after disconnecting

--- chat/bridge/bridge/main.py
+++ chat/bridge/bridge/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, WebSocket
 from fastapi.websockets import WebSocketDisconnect
 import re
 from collections import defaultdict
+from typing import Any
+import json
 
 clients = []
 channels = defaultdict(set)
@@ -23,15 +25,19 @@ async def websocket_endpoint(websocket: WebSocket):
                     channel_id = match.group(1)
                     print("Topic to subscribe:", channel_id)
                     channels[channel_id].add(websocket)
-                    await send_to_channel(channel_id, "{'type':'Message', 'message': {'content':'tst', 'username':'me', 'id':'1', 'timestamp':'01-01-1970'}}")
+                    await send_to_channel(channel_id, [{"type":"Message", "message": {"content":"tst", "username":"me", "id":"1", "timestamp":"01-01-1970"}}])
             else:
                 print(data)
     except WebSocketDisconnect:
         clients.remove(websocket)
+        for channel in channels.values():
+            if websocket in channel:
+                channel.remove(websocket)
 
 
-async def send_to_channel(channel_id: str, message: str):
-    msg = f"SEND\ndestination:{channel_id}\n\n{message}\000"
+async def send_to_channel(channel_id: str, message: Any):
+    message_json = json.dumps(message)
+    msg = f"SEND\ndestination:{channel_id}\n\n{message_json}\000"
     print(msg)
     for ws in channels[channel_id]:
         print(ws)
