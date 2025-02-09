d98d5c3a00c9a75fca295c228d0214e564eb5e62
xpakx <55586470+xpakx@users.noreply.github.com>
2025-02-09 15:20:21 +0100

    feat: let client subscribe channels

--- chat/bridge/bridge/main.py
+++ chat/bridge/bridge/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, WebSocket
 from fastapi.websockets import WebSocketDisconnect
+import re
+from collections import defaultdict
 
 clients = []
+channels = defaultdict(set)
 
 app = FastAPI()
 
@@ -12,6 +15,24 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             data = await websocket.receive_text()
-            print(data)
+            data = data.strip()
+            if data.startswith("SUBSCRIBE"):
+                match = re.search(r"destination:\s*(\S+)", data)
+                print(data)
+                if match:
+                    channel_id = match.group(1)
+                    print("Topic to subscribe:", channel_id)
+                    channels[channel_id].add(websocket)
+                    await send_to_channel(channel_id, "{'type':'Message', 'message': {'content':'tst', 'username':'me', 'id':'1', 'timestamp':'01-01-1970'}}")
+            else:
+                print(data)
     except WebSocketDisconnect:
         clients.remove(websocket)
+
+
+async def send_to_channel(channel_id: str, message: str):
+    msg = f"SEND\ndestination:{channel_id}\n\n{message}\000"
+    print(msg)
+    for ws in channels[channel_id]:
+        print(ws)
+        await ws.send_text(msg)
