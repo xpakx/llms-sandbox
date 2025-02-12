7da60cc87bb94cb1e342a7808dacfa762adff896
xpakx <55586470+xpakx@users.noreply.github.com>
2025-02-12 18:53:58 +0100

    fix: add workaround for typing in prompt

--- typing/typing/main.py
+++ typing/typing/main.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from openai import OpenAI
 from typing import Dict, Any
 import json
+from datetime import datetime
 
 def load_config(filename: str) -> Dict[str, Any]:
     with open(filename, 'r') as file:
@@ -20,10 +21,18 @@ class CalendarEvent(BaseModel):
 config = load_config("config.json")
 client = get_client(config["apiKey"])
 
+prompt = """
+         Extract the event information. Respond in valid JSON only. 
+         Your response will be automatically validated, and shouldn't contain any tokens outside JSON object (e.g. no '```json' part).
+         Format:  {"name": "name of event", "date": "01-20-1970", "participants": []}
+         """
+         
+
 completion = client.beta.chat.completions.parse(
         model="deepseek/deepseek-chat:free",
         messages=[
-            {"role": "system", "content": "Extract the event information."},
+            {"role": "system", "content": prompt},
+            {"role": "system", "content": f"Today is ${datetime.now()}"},
             {"role": "user", "content": "Alice and Bob are going to a science fair on Friday."},
             ],
         response_format=CalendarEvent,
