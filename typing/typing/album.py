d4a4f2b9f2e2be90e8e3e7126c9a8a194b3aa5ae
xpakx <55586470+xpakx@users.noreply.github.com>
2025-02-16 17:44:37 +0100

    feat: parse commandline arguments in album example

--- typing/typing/album.py
+++ typing/typing/album.py
@@ -1,4 +1,5 @@
 import sqlite3
+import argparse
 
 
 def execute_sql_file(cursor, sql_file):
@@ -15,12 +16,50 @@ def show_tables(cursor):
         print(table[0])
 
 
+def get_parser():
+    parser = argparse.ArgumentParser(description="A program to manage albums.")
+    subparsers = parser.add_subparsers(dest="command", help="Available subcommands")
+
+    parser_url = subparsers.add_parser("url", help="Manage URLs")
+    url_subparsers = parser_url.add_subparsers(dest="subcommand", help="Available subcommands")
+    parser_url_add = url_subparsers.add_parser("add", help="Add a new URL to track")
+    parser_url_add.add_argument("url", type=str, help="The URL to add")
+
+    parser_check = subparsers.add_parser("check", help="Check all URLs in the database for new albums")
+    parser_view = subparsers.add_parser("view", help="Print all albums in the database")
+    return parser
+
+
+def add_url(cursor, url):
+    print(f"Adding URL: {args.url}")
+
+
+def check_urls(cursor):
+    print("Checking all URLs in the database")
+
+
+def view(cursor):
+    print("Viewing all data in the database")
+
+
 if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+
     conn = sqlite3.connect('albums.db')
     cursor = conn.cursor()
-
     execute_sql_file(cursor, "data.sql")
     conn.commit()
+    # show_tables(cursor)
 
-    show_tables(cursor)
+    if args.command == "url" and args.subcommand == "add":
+        add_url(cursor, args.url)
+        conn.commit()
+    elif args.command == "check" or args.command == None:
+        check_urls(cursor)
+    elif args.command == "view":
+        view(cursor)
+    else:
+        parser.print_help()
+
     conn.close()
