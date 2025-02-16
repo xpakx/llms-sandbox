9fd91b4d59b666ff8221b039ce1ee4c8a037098b
xpakx <55586470+xpakx@users.noreply.github.com>
2025-02-16 17:29:46 +0100

    refactor: extract and clean methods for album example

--- typing/typing/album.py
+++ typing/typing/album.py
@@ -1,18 +1,26 @@
 import sqlite3
 
-conn = sqlite3.connect('albums.db')
-cursor = conn.cursor()
 
-with open("data.sql", 'r') as file:
-    sql_script = file.read()
-cursor.executescript(sql_script)
+def execute_sql_file(cursor, sql_file):
+    with open(sql_file, 'r') as file:
+        sql_script = file.read()
+    cursor.executescript(sql_script)
 
-conn.commit()
 
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-tables = cursor.fetchall()
-print("Tables in the database:")
-for table in tables:
-    print(table[0])
+def show_tables(cursor):
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    tables = cursor.fetchall()
+    print("Tables in the database:")
+    for table in tables:
+        print(table[0])
 
-conn.close()
+
+if __name__ == "__main__":
+    conn = sqlite3.connect('albums.db')
+    cursor = conn.cursor()
+
+    execute_sql_file(cursor, "data.sql")
+    conn.commit()
+
+    show_tables(cursor)
+    conn.close()
