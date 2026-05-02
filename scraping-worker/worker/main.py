a47c24c059f05764500910907126dee36023307e
xpakx <55586470+xpakx@users.noreply.github.com>
2026-05-02 23:31:27 +0200

    feat(webscraper): add simple task that get webpages' content

--- scraping-worker/worker/main.py
+++ scraping-worker/worker/main.py
@@ -1,20 +1,38 @@
 import asyncio
 from aioclock import Every
 from aioclock.group import Group
+import msgspec
+import requests
 
 from scheduler import get_scheduler
+
+
+class Data(msgspec.Struct):
+    url: str
 
 
 tasks = asyncio.Queue()
 scheduler = Group()
 
 
+async def add_task(data: str):
+    try:
+        config = msgspec.json.decode(data, type=Data)
+        await tasks.put(config.url)
+    except msgspec.DecodeError as e:
+        print(f"Couldn't load config file {e}")
+
+
 @scheduler.task(trigger=Every(seconds=5))
 async def test():
-    print('test')
+    url = await tasks.get()
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(response.text)
 
 
 async def run(app):
+    await tasks.put("https://example.com")
     task = asyncio.create_task(app.serve())
     await task
 
